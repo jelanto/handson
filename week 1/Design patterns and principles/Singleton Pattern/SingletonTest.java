4d1613c65a920af84dc88817498f9d771d154567
jelanto <jelanto994@gmail.com>
2026-06-22 15:39:18 +0530

    Added Week 1 Design Pattern exercises

---
+++ week 1/Design patterns and principles/Singleton Pattern/SingletonTest.java
@@ -0,0 +1,17 @@
+public class SingletonTest {
+
+    public static void main(String[] args) {
+
+        Logger logger1 = Logger.getInstance();
+        logger1.log("Application started");
+
+        Logger logger2 = Logger.getInstance();
+        logger2.log("User logged in");
+
+        if (logger1 == logger2) {
+            System.out.println("Singleton pattern verified: Only one Logger instance exists.");
+        } else {
+            System.out.println("Multiple Logger instances created.");
+        }
+    }
+}
