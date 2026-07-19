c94fca2bf454f63de3eecf96183db5ccad3c04be
jelanto <jelanto994@gmail.com>
2026-07-19 12:07:42 +0530

    Delete week 5/1 directory

--- week 5/1/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "20px" }}>
-      <h1>Welcome to the first session of React</h1>
-    </div>
-  );
-}
-
-export default App;
